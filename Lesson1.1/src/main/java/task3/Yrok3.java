4d74613530f7c4ce75491257a7360c728f77c861
Александр <kokser099@mail.ru>
2021-10-08 16:57:25 +0300

    Домашка по 3 уроку, финальный вариант

--- Lesson1.1/src/main/java/task3/Yrok3.java
+++ Lesson1.1/src/main/java/task3/Yrok3.java
@@ -11,13 +11,13 @@ public class Yrok3 {
         System.out.println(operation(1));
         System.out.println(operation(0));
         System.out.println(calculateCountOfOddElementsInMatrix(new int[]{1, 2, 3, 4, 5, 6}));
-        //calculateSumOfDiagonalElements();
+        calculateSumOfDiagonalElements();
         countDevs(103);
         foobar(6);
         foobar(10);
         foobar(15);
-        //  printMatrix();
-        printPrimeNumbers();
+        printMatrix();
+      printPrimeNumbers();
     }
 
     /**
@@ -116,43 +116,38 @@ public class Yrok3 {
             System.out.println("foobar");
         }
     }
+
     /**
      * заполнить рандомно 2-х мерный массив и посчитать сумму элементов на диагонали
      */
-    //public static void calculateSumOfDiagonalElements() {
+    public static void calculateSumOfDiagonalElements() {
     //пишем логику и выводим результат используя System.out.println
-// ЕЩЕ РАБОТАЮ НАД ЭТОЙ ЗАДАЧЕЙ, НЕ ЗАКОНЧЕНА. Не понял только насчет заполнения массива, заполнять рандомно с клавиатуры или при помощи команды рандом
-    //Scanner console = new Scanner(System.in);
-    //int x = console.nextInt();
-    // int y = console.nextInt();
 
-    // System.out.println("Введите размерность массива" + x, y);
-    // int[][] mm = new int[x][y];
-    // for (int x1 = 0; x1 < x; x1++) {
-    //     for (int y1 = 0; y1 < x; y1++) {
-    //     x1 = console.nextInt();
-    //    }
-    //  int y1 = console.nextInt();
 
-    // Scanner console = new Scanner(System.in);
-    // int x = console.nextInt();
-    //int y = console.nextInt();
-    // int[] a = new int[x][y];
+        Scanner console = new Scanner(System.in);
+        int x = console.nextInt();
+        int y = console.nextInt();
+        int[][] a = new int[x][y];
+        int[][] mas2d = new int[x][y];
+        Random rand = new Random();
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                mas2d[i][j] = rand.nextInt(10);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++)
+                System.out.print(mas2d[i][j] + "  ");
+            System.out.println();
+        }
 
-    // Random random = new Random();
-
-    // for (int i = 0; i < x; i++)
-    //  {
-    //      for (int j = 0; j < y; j++)
-    //      {
-    //          a[i] [j] = random.Next(100);
-    //          Console.Write("{0,4}", a[i] [j]);
-    //      }
-    //     Console.WriteLine();
-    //  }
-    // }
-    //}
-    //}
+        int c = 0;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++)
+                if (i == j) {
+                    c = c + mas2d[i][j];
+                }
+        }
+        System.out.print(c);
+    }
 
 
     /**
@@ -176,50 +171,78 @@ public class Yrok3 {
      * * - +
      * <p>
      * Обратите внимание, что 21% 3 == 0 и 21% 7 = 0, но выводить надо не +-, а +
-     */
-    public static void printMatrix() {
-        // тут пишем логику
-// ЕЩЕ РАБОТАЮ НАД ЭТОЙ ЗАДАЧЕЙ, НЕ ЗАКОНЧЕНА
-        Scanner scanner = new Scanner(System.in);
-int v,g;
-        v = scanner.nextInt();
-        g = scanner.nextInt();
-        int[][] ints = new int[v][g];
-        Random random = new Random();
-        for (int i = 0; i < ints.length; i++) {
-            ints[i] = random.nextInt(100);
-            if ( i % 3 == 0) {
-                System.out.println("+");
-            }
-            if (i % 7 == 0) {
-                System.out.println("-");
-            }
-            else {System.out.println("*");}
-        }
-    }
-
-
-
-
-
-    /**
+     * <p>
+     * Задача со звездочкой!
+     * Метод должен печатать все простые числа <1000
+     * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
+     * <p>
      * Задача со звездочкой!
      * Метод должен печатать все простые числа <1000
      * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
      */
+    public static void printMatrix() {
+        // тут пишем логику
+
+        Scanner scanner = new Scanner(System.in);
+        int v, g;
+        v = scanner.nextInt();
+        g = scanner.nextInt();
+        int[][] ints = new int[v][g];
+        Random random = new Random();
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < g; j++) {
+                ints[i][j] = random.nextInt(21);
+            }
+        }
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < g; j++) {
+
+                if (ints[i][j] % 3 == 0 && ints[i][j] % 7 == 0) {
+                    System.out.print("+" + "-");
+                } else if (ints[i][j] % 3 == 0) {
+                    System.out.print("+");
+                } else if (ints[i][j] % 7 == 0) {
+                    System.out.print("-");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < g; j++) {
+
+                System.out.print(ints[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+/**
+ * Задача со звездочкой!
+ * Метод должен печатать все простые числа <1000
+ * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
+ */
     public static void printPrimeNumbers() {
 
 
-            int x, y;
-            for (x = 2; x < 1001; x++) {
-                y = 0;
-                for (int i = 1; i <= x; i++) {
-                    if (x % i == 0)
-                        y++;
-                }
-                if (y <= 2)
-                    System.out.println(x);
-
+        int x, y;
+        for (x = 2; x < 1001; x++) {
+            y = 0;
+            for (int i = 1; i <= x; i++) {
+                if (x % i == 0)
+                    y++;
             }
+            if (y <= 2)
+                System.out.println(x);
+
         }
     }
+}
+
+
+
+
