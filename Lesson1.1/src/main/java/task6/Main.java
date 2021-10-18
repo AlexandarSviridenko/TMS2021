f36b0aa6dee0289f40388c2464a9a8b0197a50f9
Александр <kokser099@mail.ru>
2021-10-18 17:32:41 +0300

    застопорился на задаче по 6 уроку. В коде прописал в чем у меня проблема.

---
+++ Lesson1.1/src/main/java/task6/Main.java
@@ -0,0 +1,49 @@
+package task6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Иван", 19, "man", "Minsk");
+        Person person2 = new Person("Ольга", 19, "women", "Minsk");
+        Person person3 = new Person("Иван", 25, "man", "Minsk");
+        Person person4 = new Person("Иван", 30, "man", "Grodno");
+        Person person5 = new Person("Саша", 19, "man", "Grodno");
+        Person person6 = new Person("Иван", 30, "man", "Minsk");
+//        System.out.print("  " +person1.name);
+//        System.out.print("  " +person1.age);
+//        System.out.print("  " +person1.gender);
+//        System.out.println("  " +person1.adress);
+//        System.out.print("  " +person2.name);
+//        System.out.print("  " +person2.age);
+//        System.out.print("  " +person2.gender);
+//        System.out.println("  " +person2.adress);
+//        System.out.print("  " +person3.name);
+//        System.out.print("  " +person3.age);
+//        System.out.print("  " +person3.gender);
+//        System.out.println("  " +person3.adress);
+//        System.out.print("  " +person4.name);
+//        System.out.print("  " +person4.age);
+//        System.out.print("  " +person4.gender);
+//        System.out.println("  " +person4.adress);
+//        System.out.print("  " +person5.name);
+//        System.out.print("  " +person5.age);
+//        System.out.print("  " +person5.gender);
+//        System.out.println("  " +person5.adress);
+//        System.out.print("  " +person6.name);
+//        System.out.print("  " +person6.age);
+//        System.out.print("  " +person6.gender);
+//        System.out.print("  " +person6.adress);
+
+
+//        Person allperson = new Person();
+//        allperson.showperson1();//вывод всех людей в персоне
+//        allperson.showperson2();
+//        allperson.showperson3();
+//        allperson.showperson4();
+//        allperson.showperson5();
+//
+//        MilitaryOffice solder1 = new MilitaryOffice();
+//        PersonRegistry solder11 = new PersonRegistry)
+//        solder1.
+    }
+}
---
+++ Lesson1.1/src/main/java/task6/MilitaryOffice.java
@@ -0,0 +1,65 @@
+package task6;
+
+public class MilitaryOffice {
+
+    public static void PersonRegistry() {
+
+        int ageSolders1 = 18;
+        int ageSolders2 = 27;
+        String genderSolders = "man";
+        String adressSolders = "Minsk";
+        int ageSolders3 = 25;
+        int ageSolders4 = 27;
+        String nameSolders = "Саша";
+
+
+        public void solders1 () {
+            Person solders1 = new Person();
+            if (Person.age >= ageSolders1 && Person.age <= ageSolders2 && Person.gender == genderSolders) {
+                System.out.println(Person.name);
+            }
+        }
+        public void solders2 () {
+            Person solders2 = new Person();
+            if (Person.adress == adressSolders) {
+                System.out.println(Person.name)
+            }
+        }
+        public void solders3 () {
+            Person solders3 = new Person();
+            if (Person.age >= ageSolders3 && Person.age <= ageSolders4 && Person.gender == genderSolders) {
+                System.out.println(Person.name);
+            }
+        }
+        public void solders4 () {
+            Person solders4 = new Person();
+            if (Person.name == nameSolders) {
+                System.out.println(Person.name)
+
+
+//            //не понимаю, почему возникает ошибка когда ссылаюсь на переменные? Как решить?
+            }
+        }
+    }
+}
+//
+////        Домашка: 6 урок
+////        Необходимо написать военкомат.
+////        1) Создаем класс военкомат MilitaryOffice
+////        2) Создать в классе MilitaryOffice конструктор, который принимает PersonRegistry
+////        3) PersonRegistry содержит список людей (Person), которые могут быть призваны в армию
+////        4) Создать класс Person, который будет содержать следующие поля имя, возраст, пол, адрес(страна, город)
+////
+////        Военкомат должен уметь:
+////        a) вывести имена всех людей годных к военной службе на текущий момент времени, которые есть в PersonRegistry
+////        T.е нужно написать метод в классе MilitaryOffice, который будет отфильтровывать годных к службе призывников
+////        (годными считать мущин от 18 до 27 лет, проверяем пол и возраст)
+////        б) вывести количество годных призывников в городе Минске.
+////        в) вывести количество призывников от 25 до 27 лет
+////        г) вывести количество призывников у которых имя Александр.
+////
+////        5) Создать класс Main, наполнить PersonRegistry людьми, по желанию можно создавать людей через консоль
+//
+//
+//}
+//}
