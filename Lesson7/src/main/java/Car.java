c3cee3bbc181272744c4fa7ca837419bee7a55bd
Александр <kokser099@mail.ru>
2021-10-31 22:50:12 +0300

    Задача еще в работе не обращать внимание

--- Lesson7/src/main/java/Car.java
+++ Lesson7/src/main/java/Car.java
@@ -1,10 +1,22 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Scanner;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 
 public class Car {
     //Марка машины, год выпуска, пройденное расстояние
+    public static final engineVolume; // В ЧЕМ ОШИБКА? Не понимаю как вынести глобальную переменную
+    public static Gastank;
     private String brand;
     private int yearOfIssue;
     private int mileage;
+
 
     public Car(String brand, int yearOfIssue, int mileage) {
         this.brand = brand;
@@ -12,83 +24,80 @@ public class Car {
         this.mileage = mileage;
     }
 
-    static class Engine {
+    Engine engine = new Engine;  // Так можно было сделать? Не нравится как теперь выглядит мэйн
+    Gastank gastank = new Gastank;
+
+    static final class Engine {
 
         private int engineVolume;
 
         public Engine(int engineVolume) {
             this.engineVolume = engineVolume;
         }
-
-        public void info() {
-            System.out.println("Модель" + brand + "Год выпуска" + yearOfIssue + "Километраж" + mileage);
-        }
-
-        public void on() {
-            Gastank fuel = new Gastank();
-            System.out.println("Заведите машину. Для этого нажмите 1");
-            Scanner console = new Scanner(System.in);
-            int oncar = console.nextInt();
-            if (oncar == 1 && fuel.fuel > 0) {
-                System.out.println("Машина завелась");
-                drive();
-            } else {
-                System.out.println("Выбрано неверное число");
-                on();
-            }
-        }
-
-        public void drive() {
-            System.out.println("Машина едет");
-            off();
-        }
-
-        public void off() {
-            Gastank fuel = new Gastank();
-            System.out.println("Заглушите машину. Для этого нажмите 0");
-            Scanner console = new Scanner(System.in);
-            int offcar = console.nextInt();
-            if (offcar == 0) {
-                System.out.println("Машина заглушена");
-            } else {
-                System.out.println("Выбрано неверное число");
-                off();
-            }
-            mileage++;
-            fuel.fuel--;
-        }
-
     }
-
 
     static class Gastank {
         private int fuel;
         private int gasTankVolume;
-        private int needfuel;
+
 
         public Gastank(int fuel, int gasTankVolume) {
             this.fuel = fuel;
             this.gasTankVolume = gasTankVolume;
         }
+    }
 
+    public void infoStart() {
+        System.out.println("Модель" + brand + "Год выпуска" + yearOfIssue + "Километраж" + mileage);
+    }
 
-        public void buyFuel() {
-            System.out.println("Ваш объем бака:" + gasTankVolume + "  На данный момент в баке находится:" + fuel + "Выбирите сколько топлива залить:");
-            Scanner console = new Scanner(System.in);
-            int buyFuel = console.nextInt();
-            if (buyFuel <= gasTankVolume - fuel) {
-                fuel = fuel + buyFuel;
-                System.out.println("Вы залили в бак:" + buyFuel + "В баке теперь:" + fuel);
-            } else {
-                System.out.println("Вы не можете столько залить");
-                buyFuel();
-            }
+    public void on() {
+        System.out.println("Заведите машину. Для этого нажмите 1");
+        Scanner console = new Scanner(System.in);
+        int oncar = console.nextInt();
+        if (oncar == 1 && gastank.fuel > 0) {   //ПОЧЕМУ НЕ МОГУ ДОСТУЧАТЬСЯ ДО ПЕРЕМЕННОЙ?Объект создан в main или в даном классе так же нужно создавать?
+            System.out.println("Машина завелась");
+            drive();
+        } else {
+            System.out.println("Выбрано неверное число");
+            on();
         }
     }
 
-    public void info1() {
-        Gastank fuel = new Gastank;
-        System.out.println(brand + yearOfIssue + "Пройдено километров за все время" + mileage + "Объем бензобака" + fuel.gasTankVolume + "Осталось топлива" + fuel.fuel);
+    public void drive() {
+        System.out.println("Машина едет");
+        off();
+    }
+
+    public void off() {
+        System.out.println("Заглушите машину. Для этого нажмите 0");
+        Scanner console = new Scanner(System.in);
+        int offcar = console.nextInt();
+        if (offcar == 0) {
+            System.out.println("Машина заглушена");
+        } else {
+            System.out.println("Выбрано неверное число");
+            off();
+        }
+        mileage++;
+        gastank.fuel--;
+    }
+
+    public void buyFuel() {
+        System.out.println("Ваш объем бака:" + gastank.gasTankVolume + "  На данный момент в баке находится:" + gastank.fuel + "Выбирите сколько топлива залить:");
+        Scanner console = new Scanner(System.in);
+        int buyFuel = console.nextInt();
+        if (buyFuel <= gastank.gasTankVolume - gastank.fuel) {
+            gastank.fuel = gastank.fuel + buyFuel;
+            System.out.println("Вы залили в бак:" + buyFuel + "В баке теперь:" + gastank.fuel);
+        } else {
+            System.out.println("Вы не можете столько залить");
+            buyFuel();
+        }
+    }
+
+    public void infoEnd() {
+        System.out.println(brand + yearOfIssue + "Пройдено километров за все время" + mileage + "Объем бензобака" + gastank.gasTankVolume + "Осталось топлива" + gastank.fuel);
     }
 }
 
--- Lesson7/src/main/java/Main.java
+++ Lesson7/src/main/java/Main.java
@@ -1,17 +1,23 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Main {
+
     public static void main(String[] args) {
-        Car workCarr = new Car("ford", 1999, 905635);
-        Car.Engine insideCar = new Car.Engine(2);
-        Car.Gastank gascar = new Car.Gastank(2, 40);
+        Car myCar = new Car("ford", 1999, 905635);
+        Car.Engine engine = new Car.Engine(2);
+        Car.Gastank gastank = new Car.Gastank(2, 40);
 
 //Car.Gastank needfuel= new Car.Gastank();
 //        Car a = new Car(fuel);
 
-        insideCar.on();
-        insideCar.drive();
-        insideCar.off();
-        insideCar.info();
-        gascar.buyFuel();
-        workCarr.info();
+        myCar.on();
+        myCar.drive();
+        myCar.off();
+        myCar.infoStart();
+        myCar.buyFuel();
+        myCar.infoEnd();
     }
 }
